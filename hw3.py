324a73ad7aaf463a117fe31a59659da5780c9e69
pikerple <b07902074@ntu.edu.tw>
2020-03-29 15:21:20 +0800

    first_try

---
+++ hw3.py
@@ -0,0 +1,117 @@
+import os
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import pandas as pd
+from torch.utils.data import DataLoader, Dataset
+import time
+def readfile(path, label):
+    # label 是一個 boolean variable，代表需不需要回傳 y 值
+    image_dir = sorted(os.listdir(path))
+    x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
+    y = np.zeros((len(image_dir)), dtype=np.uint8)
+    for i, file in enumerate(image_dir):
+        img = cv2.imread(os.path.join(path, file))
+        x[i, :, :] = cv2.resize(img,(128, 128))
+        if label:
+          y[i] = int(file.split("_")[0])
+    if label:
+      return x, y
+    else:
+      return x
+workspace_dir = '../food-11'
+print("Reading data")
+train_x = np.load('train_x.npy')
+train_y = np.load('train_y.npy')
+# np.save('train_x.npy', train_x)
+# np.save('train_y.npy', train_y)
+print("Size of training data = {}".format(len(train_x)))
+#val_x, val_y = readfile(os.path.join(workspace_dir, "validation"), True)
+val_x = np.load('val_x.npy')
+val_y = np.load('val_y.npy')
+# np.save('val_x.npy', val_x)
+# np.save('val_y.npy', val_y)
+print("Size of validation data = {}".format(len(val_x)))
+test_x = readfile(os.path.join(workspace_dir, "testing"), False)
+np.save('test_x.npy', test_x)
+print("Size of Testing data = {}".format(len(test_x)))
+#training 時做 data augmentation
+train_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(), #隨機將圖片水平翻轉
+    transforms.RandomRotation(15), #隨機旋轉圖片
+    transforms.ToTensor(), #將圖片轉成 Tensor，並把數值normalize到[0,1](data normalization)
+])
+#testing 時不需做 data augmentation
+test_transform = transforms.Compose([
+    transforms.ToPILImage(),                                    
+    transforms.ToTensor(),
+])
+class ImgDataset(Dataset):
+    def __init__(self, x, y=None, transform=None):
+        self.x = x
+        # label is required to be a LongTensor
+        self.y = y
+        if y is not None:
+            self.y = torch.LongTensor(y)
+        self.transform = transform
+    def __len__(self):
+        return len(self.x)
+    def __getitem__(self, index):
+        X = self.x[index]
+        if self.transform is not None:
+            X = self.transform(X)
+        if self.y is not None:
+            Y = self.y[index]
+            return X, Y
+        else:
+            return X
+batch_size = 128
+train_set = ImgDataset(train_x, train_y, train_transform)
+val_set = ImgDataset(val_x, val_y, test_transform)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
+class Classifier(nn.Module):
+    def __init__(self):
+        super(Classifier, self).__init__()
+        #torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        #torch.nn.MaxPool2d(kernel_size, stride, padding)
+        #input 維度 [3, 128, 128]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 64, 3, 1, 1),  # [64, 128, 128]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [64, 64, 64]
+
+            nn.Conv2d(64, 128, 3, 1, 1), # [128, 64, 64]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [128, 32, 32]
+
+            nn.Conv2d(128, 256, 3, 1, 1), # [256, 32, 32]
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),      # [256, 16, 16]
+
+            nn.Conv2d(256, 512, 3, 1, 1), # [512, 16, 16]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),       # [512, 8, 8]
+            
+            nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),       # [512, 4, 4]
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(512*4*4, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 11)
+        )
+
+    def forward(self, x):
+       
